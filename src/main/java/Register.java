893ab8ef037f503d597888b8420cbba51b158c80
Shashank <shashank.expert@gmail.com>
2022-12-13 00:08:09 +0530

    register commiit in login 2 branch

---
+++ src/main/java/Register.java
@@ -0,0 +1,29 @@
+public class Register {
+
+    private String name;
+    private String password;
+
+    public Register() {
+    }
+
+    public Register(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
