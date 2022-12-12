6930ec1b6f4e426276a16e1436458fb20feaf967
Shashank <shashank.expert@gmail.com>
2022-12-13 00:51:18 +0530

    Added hascode and toString in login branch

--- src/main/java/Register.java
+++ src/main/java/Register.java
@@ -26,4 +26,22 @@ public class Register {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Register)) return false;
+
+        Register register = (Register) o;
+
+        if (getName() != null ? !getName().equals(register.getName()) : register.getName() != null) return false;
+        return getPassword() != null ? getPassword().equals(register.getPassword()) : register.getPassword() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getName() != null ? getName().hashCode() : 0;
+        result = 31 * result + (getPassword() != null ? getPassword().hashCode() : 0);
+        return result;
+    }
 }
